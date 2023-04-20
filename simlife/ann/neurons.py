f972ffa9047bfd4ceb09cb67f585cf7815e34ad2
Frederic Vogels <frederic.vogels@ucll.be>
2023-04-20 14:35:11 +0200

    RoundingNeuron class

--- simlife/ann/neurons.py
+++ simlife/ann/neurons.py
@@ -7,3 +7,20 @@ class FrontSensor:
         world = boid.world
         position_in_front_of_boid = boid.position + boid.direction
         return world[position_in_front_of_boid] is not None
+
+
+class RoundingNeuron:
+    def __init__(self, threshold=0.1):
+        self.__threshold = threshold
+        self.__last_value = 0
+
+    def feed_input(self, value):
+        self.__last_value = value
+
+    def determine_output(self):
+        if self.__last_value > self.__threshold:
+            return 1.0
+        elif self.__last_value < -self.__threshold:
+            return -1.0
+        else:
+            return 0.0
