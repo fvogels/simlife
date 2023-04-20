765c816874703b7c15b4618b309e419a86f04506
Frederic Vogels <frederic.vogels@ucll.be>
2023-04-20 18:12:58 +0200

    Timer.update -> Timer.tick

--- simlife/util/timer.py
+++ simlife/util/timer.py
@@ -3,7 +3,7 @@ class Timer:
         self.__delay = delay
         self.__elapsed_seconds = 0
 
-    def update(self, elapsed_seconds):
+    def tick(self, elapsed_seconds):
         self.__elapsed_seconds += elapsed_seconds
         if self.__elapsed_seconds >= self.__delay:
             self.__elapsed_seconds -= self.__delay
