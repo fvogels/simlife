27d429afb8d32a375f54c6b1668f1eba2f8c4df4
Frederic Vogels <frederic.vogels@ucll.be>
2023-04-20 18:25:32 +0200

    Simulation

---
+++ simlife/simulation/simulation.py
@@ -0,0 +1,17 @@
+class Simulation:
+    def __init__(self, world):
+        self.__world = world
+
+    @property
+    def world(self):
+        return self.__world
+
+    def compute_next(self):
+        boids = [cell for cell in self.__world if cell is not None]
+        for boid in boids:
+            decision = boid.decide_action()
+            if decision.movement_direction:
+                old_position = boid.position
+                new_position = old_position + decision.movement_direction
+                if self.__world.is_valid_position(new_position) and self.__world[new_position] is None:
+                    self.__world.move_entity(old_position, new_position)
