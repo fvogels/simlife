6acc2907bd7544c8deb82b393ca45881be195e69
Frederic Vogels <frederic.vogels@ucll.be>
2023-04-20 18:12:32 +0200

    Timer class

---
+++ simlife/util/timer.py
@@ -0,0 +1,12 @@
+class Timer:
+    def __init__(self, delay):
+        self.__delay = delay
+        self.__elapsed_seconds = 0
+
+    def update(self, elapsed_seconds):
+        self.__elapsed_seconds += elapsed_seconds
+        if self.__elapsed_seconds >= self.__delay:
+            self.__elapsed_seconds -= self.__delay
+            return True
+        else:
+            return False
