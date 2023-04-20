e7421396d75169c23987bdedd3cfd76070b3d4ef
Frederic Vogels <frederic.vogels@ucll.be>
2023-04-20 18:25:27 +0200

    Boid

---
+++ simlife/simulation/boid.py
@@ -0,0 +1,29 @@
+class Boid:
+    def __init__(self, world, position, orientation, artificial_intelligence):
+        self.__orientation = orientation
+        self.__world = world
+        self.__position = position
+        self.__artificial_intelligence = artificial_intelligence
+
+    @property
+    def world(self):
+        return self.__world
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, new_position):
+        self.__position = new_position
+
+    @property
+    def orientation(self):
+        return self.__orientation
+
+    @orientation.setter
+    def orientation(self, new_orientation):
+        self.__orientation = new_orientation
+
+    def decide_action(self):
+        return self.__artificial_intelligence.decide_action()
