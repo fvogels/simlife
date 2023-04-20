a8b92714c8465d2a557b5f6fdfb895a19ca062cd
Frederic Vogels <frederic.vogels@ucll.be>
2023-04-20 21:45:02 +0200

    vertical movement in AI

--- simlife/simulation/ai.py
+++ simlife/simulation/ai.py
@@ -8,18 +8,29 @@ class ArtificialIntelligence:
         builder = NeuralNetworkBuilder()
         constant = ConstantNeuron(1.0)
         self.__horizontal_movement_neuron = SignNeuron(0.1)
+        self.__vertical_movement_neuron = SignNeuron(0.1)
         builder.connect(constant, self.__horizontal_movement_neuron, 1.0)
+        builder.connect(constant, self.__vertical_movement_neuron, 1.0)
         self.__neural_network = builder.build()
 
     def decide_action(self):
         self.__neural_network.update()
+        movement = self.__decide_movement()
+        return Decision(movement_direction=movement)
+
+    def __decide_movement(self):
+        result = CENTER
         horizontal_movement_decision = self.__horizontal_movement_neuron.determine_output()
         if horizontal_movement_decision == 1.0:
-            return Decision(movement_direction=EAST)
+            result += EAST
         elif horizontal_movement_decision == -1.0:
-            return Decision(movement_direction=WEST)
-        else:
-            return Decision(movement_direction=CENTER)
+            result += WEST
+        vertical_movement_decision = self.__vertical_movement_neuron.determine_output()
+        if vertical_movement_decision == 1.0:
+            result += SOUTH
+        elif vertical_movement_decision == -1.0:
+            result += NORTH
+        return result
 
 
 class Decision:
