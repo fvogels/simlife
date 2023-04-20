839a4a797d51c303736ee00f3d7e796d89022706
Frederic Vogels <frederic.vogels@ucll.be>
2023-04-20 15:46:15 +0200

    neuron tests

---
+++ tests/neuron_tests.py
@@ -0,0 +1,22 @@
+import pytest
+from simlife.ann.neurons import *
+
+
+
+@pytest.mark.parametrize('constant', [0, 0.1, -0.5])
+def test_constant(constant):
+    neuron = ConstantNeuron(constant)
+    assert constant == neuron.determine_output()
+
+
+@pytest.mark.parametrize('threshold, input, output', [
+    (0.1, 0.0, 0.0),
+    (0.1, 0.2, 1.0),
+    (0.1, -0.2, -1.0),
+    (0.2, 0.1, 0.0),
+    (0.2, -0.1, 0.0),
+])
+def test_rounding_neuron(threshold, input, output):
+    neuron = RoundingNeuron(0.1)
+    neuron.feed_input(input)
+    assert output == neuron.determine_output()
