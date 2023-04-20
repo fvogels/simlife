8098cb70ab9cbc41faf02a2e3e27754fe2e35e44
Frederic Vogels <frederic.vogels@ucll.be>
2023-04-20 18:25:17 +0200

    World

--- simlife/simulation/world.py
+++ simlife/simulation/world.py
@@ -13,5 +13,30 @@ class World:
     def height(self):
         return self.__grid.height
 
+    def __iter__(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                position = Position(x, y)
+                yield self.__grid[position]
+
     def __getitem__(self, position):
         return self.__grid[position]
+
+    def __setitem__(self, position, value):
+        self.__grid[position] = value
+
+    def add_entity(self, entity):
+        self[entity.position] = entity
+
+    def move_entity(self, origin, destination):
+        assert self.is_valid_position(origin)
+        assert self.is_valid_position(destination)
+        assert self[origin] is not None
+        assert self[destination] is None
+        entity = self[origin]
+        self[origin] = None
+        self[destination] = entity
+        entity.position = destination
+
+    def is_valid_position(self, position):
+        return self.__grid.is_valid_position(position)
