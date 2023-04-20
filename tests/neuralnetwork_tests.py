dd27e3b99064075751d7cf044ffd9054832122f4
Frederic Vogels <frederic.vogels@ucll.be>
2023-04-20 16:25:43 +0200

    neural network tests

--- tests/neuralnetwork_tests.py
+++ tests/neuralnetwork_tests.py
@@ -70,3 +70,28 @@ def test_network_with_two_layers(constant1, constant2, threshold, weight1, weigh
     network.update()
 
     assert rounder.determine_output() == output
+
+
+@pytest.mark.parametrize('constant, threshold1, threshold2, weight1, weight2, output', [
+    *(
+        (c, t1, t2, w1, w2, 1.0)
+        for c in [0.2, 0.3, 0.5, 1.0]
+        for t1 in [0.0, 0.1, 0.4]
+        for t2 in [0.0, 0.1, 0.4]
+        for w1 in [0.1, 0.4, 0.9]
+        for w2 in [0.1, 0.4, 0.9]
+        if c * w1 > t1 and w2 > t2
+    ),
+])
+def test_network_with_three_layers(constant, threshold1, threshold2, weight1, weight2, output):
+    builder = NeuralNetworkBuilder()
+    constant = ConstantNeuron(constant)
+    rounder1 = RoundingNeuron(threshold1)
+    rounder2 = RoundingNeuron(threshold2)
+    builder.connect(constant, rounder1, weight1)
+    builder.connect(rounder1, rounder2, weight2)
+    network = builder.build()
+
+    network.update()
+
+    assert rounder2.determine_output() == output
