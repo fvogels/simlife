c68294504d437ec688e17df18eeabba376f014a0
Frederic Vogels <frederic.vogels@ucll.be>
2023-04-21 13:29:11 +0200

    simulation takes boid orientation in account

--- simlife/simulation/simulation.py
+++ simlife/simulation/simulation.py
@@ -10,8 +10,9 @@ class Simulation:
         boids = [cell for cell in self.__world if cell is not None]
         for boid in boids:
             decision = boid.decide_action()
+            boid.orientation = boid.orientation.rotate(decision.rotation)
             if decision.movement_direction:
                 old_position = boid.position
-                new_position = old_position + decision.movement_direction # .rotate(boid.orientation)
+                new_position = old_position + decision.movement_direction.rotate(boid.orientation)
                 if self.__world.is_valid_position(new_position) and self.__world[new_position] is None:
                     self.__world.move_entity(old_position, new_position)
