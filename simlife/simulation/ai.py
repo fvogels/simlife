27a498571ed6123f9c1d3bc8e811e9cb808212b4
Frederic Vogels <frederic.vogels@ucll.be>
2023-04-21 10:51:29 +0200

    ai creation based on dna

--- simlife/simulation/ai.py
+++ simlife/simulation/ai.py
@@ -1,42 +1,41 @@
 from simlife.ann.neuralnetwork import *
 from simlife.ann.neurons import *
 from simlife.util.direction import *
+from simlife.util.orientation import *
 
 
 class ArtificialIntelligence:
-    def __init__(self):
+    def __init__(self, dna, boid):
+        def decider():
+            decision = Decision()
+            decision.movement_direction += horizontal_movement_neuron.determine_output()
+            decision.movement_direction += vertical_movement_neuron.determine_output()
+            return decision
+
         builder = NeuralNetworkBuilder()
-        constant = ConstantNeuron(1.0)
-        self.__horizontal_movement_neuron = SignNeuron(0.1)
-        self.__vertical_movement_neuron = SignNeuron(0.1)
-        builder.connect(constant, self.__horizontal_movement_neuron, 1.0)
-        builder.connect(constant, self.__vertical_movement_neuron, 1.0)
+        input_layer = [
+            ConstantNeuron(1.0),
+            FrontSensor(boid),
+        ]
+        output_layer = [
+            horizontal_movement_neuron := ClassifierNeuron(negative_value=WEST.to_direction(), zero_value=Direction(0, 0), positive_value=EAST.to_direction()),
+            vertical_movement_neuron := ClassifierNeuron(negative_value=NORTH.to_direction(), zero_value=Direction(0, 0), positive_value=SOUTH.to_direction())
+        ]
+
+        for index, (input_neuron, output_neuron) in enumerate((i, o) for i in input_layer for o in output_layer):
+            builder.connect(input_neuron, output_neuron, dna[index])
+
         self.__neural_network = builder.build()
+        self.__decider = decider
 
     def decide_action(self):
         self.__neural_network.update()
-        movement = self.__decide_movement()
-        return Decision(movement_direction=movement)
+        return self.__decider()
 
-    def __decide_movement(self):
-        result = CENTER
-        horizontal_movement_decision = self.__horizontal_movement_neuron.determine_output()
-        if horizontal_movement_decision == 1.0:
-            result += EAST
-        elif horizontal_movement_decision == -1.0:
-            result += WEST
-        vertical_movement_decision = self.__vertical_movement_neuron.determine_output()
-        if vertical_movement_decision == 1.0:
-            result += SOUTH
-        elif vertical_movement_decision == -1.0:
-            result += NORTH
-        return result
 
+def ai_factory(dna):
+    return lambda boid: ArtificialIntelligence(dna, boid)
 
 class Decision:
-    def __init__(self, *, movement_direction):
-        self.__movement_direction = movement_direction
-
-    @property
-    def movement_direction(self):
-        return self.__movement_direction
+    def __init__(self):
+        self.movement_direction = Direction(0, 0)
