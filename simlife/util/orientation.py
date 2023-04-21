8caefa61d90153bc142424db3135c6263fc5db79
Frederic Vogels <frederic.vogels@ucll.be>
2023-04-21 13:28:22 +0200

    new Orientation implementation

--- simlife/util/orientation.py
+++ simlife/util/orientation.py
@@ -1,27 +1,24 @@
 import simlife.util.direction as direction
 
 
-class _North:
+class _Orientation:
+    def __init__(self, index):
+        self.__index = index
+
     def to_direction(self):
-        return direction.Direction(0, -1)
+        return [
+            direction.Direction(0, -1),
+            direction.Direction(1, 0),
+            direction.Direction(0, 1),
+            direction.Direction(-1, 0),
+        ][self.__index]
+
+    def rotate(self, other):
+        idx = (self.__index + other.__index) % 4
+        return [NORTH, EAST, SOUTH, WEST][idx]
 
 
-class _South:
-    def to_direction(self):
-        return direction.Direction(0, 1)
-
-
-class _East:
-    def to_direction(self):
-        return direction.Direction(1, 0)
-
-
-class _West:
-    def to_direction(self):
-        return direction.Direction(-1, 0)
-
-
-NORTH = _North()
-EAST = _East()
-SOUTH = _South()
-WEST = _West()
+NORTH = _Orientation(0)
+EAST = _Orientation(1)
+SOUTH = _Orientation(2)
+WEST = _Orientation(3)
