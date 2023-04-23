8725ea3f21fbec3a2082fc970718e74261c005e4
Frederic Vogels <frederic.vogels@ucll.be>
2023-04-23 10:45:36 +0200

    extra neuron tests

--- tests/neuron_tests.py
+++ tests/neuron_tests.py
@@ -4,10 +4,16 @@ from simlife.util.position import *
 from simlife.util.orientation import *
 
 
+class WorldStub:
+    def __init__(self, *, width=128, height=128):
+        self.width = width
+        self.height = height
+
 class BoidStub:
-    def __init__(self, *, position=None, orientation=None):
+    def __init__(self, *, position=None, orientation=None, world=None):
         self.position = position or Position(0, 0)
         self.orientation = orientation or NORTH
+        self.world = world or WorldStub()
 
 
 
@@ -51,4 +57,40 @@ def test_horizontal_orientation_sensor(orientation, expected):
 def test_vertical_orientation_sensor(orientation, expected):
     boid = BoidStub(orientation=orientation)
     neuron = VerticalOrientationSensor(boid)
+    assert neuron.determine_output() == expected
+
+
+@pytest.mark.parametrize('width, x, y, expected', [
+    *(
+        (128, 0, y, -1.0)
+        for y in range(0, 128)
+    ),
+    *(
+        (128, 127, y, 1.0)
+        for y in range(0, 128)
+    ),
+    (11, 5, 0, 0.0),
+])
+def test_latitude_sensor(width, x, y, expected):
+    world = WorldStub(width=width)
+    boid = BoidStub(position=Position(x, y), world=world)
+    neuron = LatitudeSensor(boid)
+    assert neuron.determine_output() == expected
+
+
+@pytest.mark.parametrize('height, x, y, expected', [
+    *(
+        (128, x, 0, -1.0)
+        for x in range(0, 128)
+    ),
+    *(
+        (128, x, 127, 1.0)
+        for x in range(0, 128)
+    ),
+    (11, 0, 5, 0.0),
+])
+def test_longitude_sensor(height, x, y, expected):
+    world = WorldStub(height=height)
+    boid = BoidStub(position=Position(x, y), world=world)
+    neuron = LongitudeSensor(boid)
     assert neuron.determine_output() == expected
