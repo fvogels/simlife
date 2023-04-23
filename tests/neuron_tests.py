002629bac395d184a107a093f1c466a213a7c089
Frederic Vogels <frederic.vogels@ucll.be>
2023-04-23 09:50:13 +0200

    extra neuron tests

--- tests/neuron_tests.py
+++ tests/neuron_tests.py
@@ -1,5 +1,13 @@
 import pytest
 from simlife.ann.neurons import *
+from simlife.util.position import *
+from simlife.util.orientation import *
+
+
+class BoidStub:
+    def __init__(self, *, position=None, orientation=None):
+        self.position = position or Position(0, 0)
+        self.orientation = orientation or NORTH
 
 
 
@@ -16,7 +24,19 @@ def test_constant(constant):
     (0.2, 0.1, 0.0),
     (0.2, -0.1, 0.0),
 ])
-def test_rounding_neuron(threshold, input, output):
-    neuron = SignNeuron(0.1)
+def test_sign_neuron(threshold, input, output):
+    neuron = SignNeuron(threshold)
     neuron.feed_input(input)
     assert output == neuron.determine_output()
+
+
+@pytest.mark.parametrize('orientation, expected', [
+    (NORTH, 0.0),
+    (SOUTH, 0.0),
+    (EAST, 1.0),
+    (WEST, -1.0),
+])
+def test_horizontal_orientation_sensor(orientation, expected):
+    boid = BoidStub(orientation=orientation)
+    neuron = HorizontalOrientationSensor(boid)
+    assert neuron.determine_output() == expected
